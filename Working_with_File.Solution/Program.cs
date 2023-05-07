0dc6c896c3c2b691a2d9f00f66233ba3bbf00286
IlhombekToshqulov <ilxomdeveloper@gmail.com>
2023-05-07 15:14:06 +0500

    CodeRob

---
+++ Working_with_File.Solution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string lines = File.ReadAllText(@"C:\Users\E-MaxPCShop\Documents\.Net Vocabulary.txt");
+        string[] words = lines.Split(' ');
+        string[] result = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            result[i] = words[i].Trim();
+        }
+        string[] answer = result;
+
+        string json = JsonSerializer.Serialize(answer);
+        Console.WriteLine(json);
+
+        StreamWriter sw = new StreamWriter("d://Documents/json.txt");
+        sw.WriteLine(json);
+        sw.Close();
+    }
+}
