c7d15a2ced12feb2bfa11eb1158d432ff3c07591
IlhombekToshqulov <ilxomdeveloper@gmail.com>
2023-05-16 09:44:10 +0500

    Create:Environment

---
+++ CreateXML/XML.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace CreateXML
+{
+     class XML
+    {
+        static void Main(string[] args)
+        {
+            XElement person = new XElement("Talabalar",
+                new XElement("Talaba"),
+                new XElement("Id", 1),
+                new XElement("Name", "Ilhom"),
+                new XElement("Familiya", "Toshqulov"));
+
+            person.Save("D://Documents/personxml.xml");
+        }
+    }
+}
